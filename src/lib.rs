3bddf17f0d3173ab449038cd7a3b569327b11d53
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2018-06-23 02:21:38 +0200

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #![feature(core_intrinsics, link_llvm_intrinsics)]
-#![feature(stdsimd)]
 #![feature(test)]
 
 #[macro_use] extern crate lazy_static;
