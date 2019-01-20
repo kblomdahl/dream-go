37d931d119fc984473e80cdb2cd30b1aaf9874c9
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2019-01-20 19:52:45 +0100

    Optimize docker volume caching schema

--- contrib/trainer/start_dev_container.sh
+++ contrib/trainer/start_dev_container.sh
@@ -5,6 +5,6 @@
 
 exec nvidia-docker run \
 	--shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
-	--net host -it --rm -v "`pwd`:/app" \
+	--net host -it --rm -v "`pwd`:/app:cached" \
 	-e CUDA_VISIBLE_DEVICES=0 \
 	$(eval docker build -q .)
