b05c827f63d128bfe464f68a3dfc3e22f9ef957d
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2019-01-21 01:53:00 +0100

    Improve the CLOP engine script
    Make the following changes to the CLOP engine script:
    - Score games in a sub-process (for better parallel)
    - Re-use `dream_go` processes between games (avoid duplicate loading
      of network weights).
    - Early-exit in case an engine wins too many games.
    - Log all output to `run_dreamgo.log` for debugging crashes

--- contrib/clop/run_dreamgo.py
+++ contrib/clop/run_dreamgo.py
@@ -39,11 +39,15 @@ W
 """
 
 from datetime import datetime
-from subprocess import Popen, PIPE, DEVNULL
+from multiprocessing import Pool, Queue
+from subprocess import Popen, PIPE, DEVNULL, STDOUT
 from random import random
+import re
 
 import sys
 import os
+
+LOG = open('run_dreamgo.log', 'a')
 
 GTP_TABLE = {
     'a': 'a',
@@ -93,121 +97,114 @@ def gtp_to_sgf(color, gtp_move):
 
     return ';{}[{}{}]'.format(color, GTP_TABLE[x], GTP_TABLE[y])
 
-def play_game(engine_1, engine_2, environ_1, environ_2):
+
+def write_command(to, line_nr, s):
+    """ Write a command to the given process and returns the response """
+    to.stdin.write("{} {}".format(line_nr, s))
+    to.stdin.flush()
+
+    for original_line in to.stdout:
+        print(original_line.rstrip(), file=LOG, flush=True)
+        line = original_line.strip().lower()
+
+        if line.startswith('=' + str(line_nr)):
+            return line.split(' ', maxsplit=2)[-1].strip()
+        elif line.startswith('?' + str(line_nr)):
+            break
+
+    return None
+
+
+def play_game(proc_1, proc_2):
     """
     Play a game of the two given engines and returns `W` if the first
     engine won, otherwise `L`.
     """
-    proc_1 = Popen(engine_1, shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, env=environ_1)
-    proc_2 = Popen(engine_2, shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, env=environ_2)
-
     def write_both(s):
         """ Write the given bytes to both of the engines. """
-        proc_1.stdin.write(s)
-        proc_2.stdin.write(s)
+        write_command(proc_1, 1000, s)
+        write_command(proc_2, 1000, s)
 
-    def write_command(to, line_nr, s):
-        """ Write a command to the given process and returns the response """
-        to.stdin.write("{} {}".format(line_nr, s).encode('utf-8'))
-        to.stdin.flush()
+    write_both('komi 7.5\n')
+    write_both('boardsize 19\n')
+    write_both('clear_board\n')
 
-        for line in to.stdout:
-            line = line.decode('utf-8').strip().lower()
+    if random() < 0.5:
+        black = (proc_1, 'B', 'W')
+        white = (proc_2, 'W', 'L')
+    else:
+        black = (proc_2, 'B', 'L')
+        white = (proc_1, 'W', 'W')
 
-            if line.startswith('=' + str(line_nr)):
-                return line
-            elif line.startswith('?' + str(line_nr)):
-                break
+    turns = [black, white]
+    all_moves = []
+    pass_count = 0
+    move_count = 0
 
-        return None
+    while move_count < 722 and pass_count < 2:
+        current, other = turns[0], turns[1]
+        line_nr = move_count + 1000
 
-    try:
-        write_both(b'komi 7.5\n')
-        write_both(b'boardsize 19\n')
-        write_both(b'clear_board\n')
-
-        if random() < 0.5:
-            black = (proc_1, 'B', 'W')
-            white = (proc_2, 'W', 'L')
-        else:
-            black = (proc_2, 'B', 'L')
-            white = (proc_1, 'W', 'W')
-
-        turns = [black, white]
-        all_moves = []
-        pass_count = 0
-        move_count = 0
-
-        while move_count < 722 and pass_count < 2:
-            current, other = turns[0], turns[1]
-            line_nr = move_count + 1000
-
-            response = write_command(current[0], line_nr, 'genmove {}\n'.format(current[1]))
-            if response:
-                move = response.split(' ')[1]
-
-                if move == 'pass':
-                    pass_count += 1
-                elif move == 'resign':
-                    return other[2], other[1], all_moves  # if an engine resign then it loses
-                else:
-                    all_moves.append((current[1], move))
-                    pass_count = 0
-
-                    # play this move to the other engine
-                    write_command(other[0], line_nr, 'play {} {}\n'.format(current[1], move))
+        move = write_command(current[0], line_nr, 'genmove {}\n'.format(current[1]))
+        if move:
+            if move == 'pass':
+                pass_count += 1
+            elif move == 'resign':
+                # if an engine resign then it loses
+                return all_moves, (other[1] + '+resign'), black[2], white[2]
             else:
-                return other[2], other[1], all_moves  # if an engine encounter an error then it loses
+                all_moves.append((current[1], move))
+                pass_count = 0
 
-            # flip the current player
-            turns = [turns[1], turns[0]]
-            move_count += 1
-
-        proc_1.stdin.write(b'quit\n')
-        proc_2.stdin.write(b'quit\n')
-
-        # we use GNU Go to determine the winner so that no complicated scoring algorithm,
-        # including figuring out if a group is alive or not, has to be implemented in
-        # the referee.
-        #
-        # We use the `final_score` variant here to avoid any incorrect scores due to
-        # complicated situations that `estimate_score` fail to take into account.
-        gnugo = Popen([
-            '/usr/games/gnugo',
-            '--chinese-rules',
-            '--positional-superko',
-            '--mode', 'gtp'
-        ], stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
-
-        gnugo.stdin.write(b'komi 7.5\n')
-        gnugo.stdin.write(b'boardsize 19\n')
-        gnugo.stdin.write(b'clear_board\n')
-
-        for (i, (color, move)) in enumerate(all_moves):
-            write_command(gnugo, 1000 + i, 'play {} {}\n'.format(color, move))
-
-        re = write_command(gnugo, 2000, 'final_score\n')
-        if not re:
-            return None, None, None  # cannot determine winner
-
-        re = re.split(' ')[1].upper()  # trim away the id prefix
-
-        gnugo.stdin.write(b'quit\n')
-        gnugo.communicate()
-
-        if re.startswith('b') or re.startswith('B'):
-            return black[2], re, all_moves
+                # play this move to the other engine
+                write_command(other[0], line_nr, 'play {} {}\n'.format(current[1], move))
         else:
-            return white[2], re, all_moves
-    finally:
-        # terminate the engines the hard way if they have not died already
-        if not proc_1.poll():
-            proc_1.kill()
-            proc_1.communicate()
-        if not proc_2.poll():
-            proc_2.kill()
-            proc_2.communicate()
+            # if an engine encounter an error then it loses
+            return all_moves, (other[1] + '+resign'), black[2], white[2]
 
+        # flip the current player
+        turns = [turns[1], turns[0]]
+        move_count += 1
+
+    return all_moves, None, black[2], white[2]
+
+
+def score_game(all_moves, black_result, white_result):
+    """ We use GNU Go to determine the winner so that no complicated scoring algorithm,
+    including figuring out if a group is alive or not, has to be implemented in
+    the referee.
+
+    We use the `final_score` variant here to avoid any incorrect scores due to
+    complicated situations that `estimate_score` fail to take into account.
+    """
+    gnugo = Popen([
+        '/usr/games/gnugo',
+        '--chinese-rules',
+        '--positional-superko',
+        '--mode', 'gtp'
+    ], stdin=PIPE, stdout=PIPE, stderr=DEVNULL, encoding='utf-8')
+
+    gnugo.stdin.write('komi 7.5\n')
+    gnugo.stdin.write('boardsize 19\n')
+    gnugo.stdin.write('clear_board\n')
+
+    for (i, (color, move)) in enumerate(all_moves):
+        write_command(gnugo, 1000 + i, 'play {} {}\n'.format(color, move))
+
+    re = write_command(gnugo, 2000, 'final_score\n')
+    if not re:
+        return None, None, None  # cannot determine winner
+
+    gnugo.stdin.write('quit\n')
+    gnugo.communicate()
+
+    return judge_result(re, black_result, white_result), re, all_moves
+
+def judge_result(re, black_result, white_result):
+    if re.startswith('b') or re.startswith('B'):
+        return black_result
+    else:
+        return white_result
 
 def main():
     """ Main function """
@@ -217,39 +214,78 @@ def main():
     extra = {}
 
     for i in range(3, len(sys.argv), 2):
-        extra[sys.argv[i]] = sys.argv[i+1]
+        key = sys.argv[i]
+        is_intp = re.match(r'([a-zA-Z_]+)\[([0-9]+)\]', key)
 
-    # play an odd number of games, and whomever wins the most is considered
-    # the overall winner
+        if is_intp:
+            extra.setdefault(is_intp[1], []).append('{},{}'.format(is_intp[2], sys.argv[i+1]))
+        else:
+            extra[key] = sys.argv[i+1]
+
+    extra = { key: ':'.join(value) if isinstance(value, list) else value for key, value in extra.items() }
+
     cwd = os.getcwd()
-    results = {}
+    proc_1 = Popen(cwd + '/bin/dream_go --num-rollout 1600', shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, env={**environ, **extra}, encoding='utf-8')
+    proc_2 = Popen(cwd + '/bin/dream_go --num-rollout 1600', shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, env=environ, encoding='utf-8')
 
-    for _ in range(5):
-        result, winner, moves = play_game(
-            cwd + "/bin/dream_go --num-rollout 800",  # engine to be optimized
-            cwd + "/bin/dream_go --num-rollout 800",  # opponent
-            {**environ, **extra},
-            environ
-        )
+    try:
+        # play an odd number of games, and whomever wins the most is considered
+        # the overall winner
+        num_samples = 5
+        results = {}
 
-        if result is not None:
-            results[result] = results.get(result, 0) + 1
+        with Pool(processes=num_samples) as pool:
+            def accumulate_winner(args):
+                """ Accumulate the winner of each game into `results` """
+                result, winner, all_moves = args
 
-        # save the final game as SGF
-        if moves:
-            filename = datetime.now().strftime('%Y%m%d.%H%M%S.%f')
+                if result is not None:
+                    results[result] = results.get(result, 0) + 1
 
-            with open('games/' + filename + '.' + result + '.sgf', 'w') as sgf:
-                sgf.write('(;GM[1]FF[4]SZ[19]RU[Chinese]KM[7.5]')
-                sgf.write('RE[{}]'.format(winner))
-                sgf.write('C[{}\n{}]'.format(result, str(extra)))
+                # save the final game as SGF
+                if all_moves:
+                    filename = datetime.now().strftime('%Y%m%d.%H%M%S.%f')
 
-                for color, move in moves:
-                    sgf.write(gtp_to_sgf(color, move))
+                    with open('games/' + filename + '.' + result + '.sgf', 'w') as sgf:
+                        sgf.write('(;GM[1]FF[4]SZ[19]RU[Chinese]KM[7.5]')
+                        sgf.write('RE[{}]'.format(winner))
+                        sgf.write('C[{}\n{}]'.format(result, str(extra)))
 
-                sgf.write(')\n')
+                        for color, move in all_moves:
+                            sgf.write(gtp_to_sgf(color, move))
 
+                        sgf.write(')\n')
 
-    print(max(results.keys(), key=lambda key: results[key]))
+            for _ in range(num_samples):
+                # early exit if any one engine has already won
+                if len(results) >= num_samples/2 and max(results.values()) >= num_samples/2:
+                    break
+
+                moves, verdict, black_result, white_result = play_game(proc_1, proc_2)
+
+                if not verdict:
+                    pool.apply_async(score_game, (moves, black_result, white_result), callback=accumulate_winner)
+                else:
+                    accumulate_winner((judge_result(verdict, black_result, white_result), verdict, moves))
+
+            write_command(proc_1, '1', 'quit\n')
+            write_command(proc_2, '1', 'quit\n')
+
+            pool.close()
+            pool.join()
+
+        print(max(results.keys(), key=lambda key: results[key]))
+    finally:
+        # terminate the engines the hard way if they have not died already
+        if not proc_1.poll():
+            proc_1.kill()
+            proc_1.communicate()
+        if not proc_2.poll():
+            proc_2.kill()
+            proc_2.communicate()
+
+        LOG.close()
+        LOG.flush()
+
 
 main()
