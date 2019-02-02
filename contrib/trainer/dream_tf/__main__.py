8ae116439c077bbba8f8909ddae47994a76c71e3
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2019-02-03 00:53:30 +0100

    Set shuffle buffer size to 524,288
    Oops... I accidentally left a debug value in the shuffle buffer size.

--- contrib/trainer/dream_tf/__main__.py
+++ contrib/trainer/dream_tf/__main__.py
@@ -500,7 +500,7 @@ def get_dataset(files, batch_size=1, is_training=True):
         dataset = dataset.filter(_illegal_policy)
         dataset = dataset.map(_fix_shape)
         if is_training:
-            dataset = dataset.apply(tf.data.experimental.shuffle_and_repeat(10000))  # 524288
+            dataset = dataset.apply(tf.data.experimental.shuffle_and_repeat(524288))
             dataset = dataset.map(_augment, num_parallel_calls=4)
             dataset = dataset.map(_fix_history, num_parallel_calls=4)
         dataset = dataset.batch(batch_size)
