2642f5bae2a64b07433333a2a0d2bc8e51942726
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2021-03-15 02:05:19 +0100

    Use the number of rollouts specified in the config during benchmarks

--- src/dg/bench/mcts.rs
+++ src/dg/bench/mcts.rs
@@ -14,12 +14,13 @@
 
 use bench::{Benchmark, BenchmarkExecutor, PredictState1};
 use dg_go::utils::sgf::SgfEntry;
-use dg_mcts::predict_service::PredictState;
-use dg_mcts::time_control::RolloutLimit;
-use dg_mcts::predict;
 use dg_mcts::options::StandardSearch;
 use dg_mcts::parallel::{self, ServiceGuard};
+use dg_mcts::predict_service::PredictState;
+use dg_mcts::predict;
+use dg_mcts::time_control::RolloutLimit;
 use dg_nn::Network;
+use dg_utils::config;
 
 pub struct MctsBenchmarkExecutor {
     #[allow(dead_code)]
@@ -42,14 +43,14 @@ impl BenchmarkExecutor for MctsBenchmarkExecutor {
         let (_value, _, tree) = predict(
             &self.server_lock,
             Box::new(StandardSearch::default()),
-            RolloutLimit::new(800),
+            RolloutLimit::new(usize::from(*config::NUM_ROLLOUT)),
             None,
             &entry.board,
             entry.color
         ).unwrap();
 
         tree.total_count as usize
-    }    
+    }
 }
 
 pub type MctsBenchmark = Benchmark<MctsBenchmarkExecutor>;
