13113e3fd533b54d9e49a00c7534b3329d8e4dcf
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2021-07-10 13:42:41 +0200

    Re-write the MLP Mixer blocks to use batch normalization relu activation

--- contrib/trainer/dream_tf/layers/mixer_block.py
+++ contrib/trainer/dream_tf/layers/mixer_block.py
@@ -19,9 +19,9 @@
 # SOFTWARE.
 
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, LayerNormalization, Reshape
+from tensorflow.keras.layers import Reshape
 
-from .to_dict import tensor_to_dict
+from .batch_norm import BatchNormDense
 from .recompute_grad import recompute_grad
 
 class MlpBlock(tf.keras.layers.Layer):
@@ -32,37 +32,40 @@ class MlpBlock(tf.keras.layers.Layer):
 
     def as_dict(self, prefix):
         return {
-            f'{prefix}/linear_1:0': tensor_to_dict(self.dense_1.kernel),
-            f'{prefix}/linear_1/offset:0': tensor_to_dict(self.dense_1.bias),
-            f'{prefix}/linear_2:0': tensor_to_dict(self.dense_2.kernel),
-            f'{prefix}/linear_2/offset:0': tensor_to_dict(self.dense_2.bias),
+            **self.dense_1.as_dict(f'{prefix}/linear_1'),
+            **self.dense_2.as_dict(f'{prefix}/linear_2')
         }
 
     def build(self, input_shapes):
-        self.dense_1 = Dense(self.mlp_dims, activation='relu')
-        self.dense_2 = Dense(input_shapes[-1], activation=None)
+        self.dense_1 = BatchNormDense(self.mlp_dims)
+        self.dense_2 = BatchNormDense(input_shapes[-1])
 
-    def call(self, x):
-        y = self.dense_1(x)
-        return self.dense_2(y)
+    def call(self, x, training=True, is_recomputing=False):
+        y = tf.nn.relu(self.dense_1(x, training=training, is_recomputing=is_recomputing))
+        return self.dense_2(y, training=training, is_recomputing=is_recomputing)
 
 class MixerBlock(tf.keras.layers.Layer):
+    """ https://arxiv.org/abs/2105.01601 """
+
     def __init__(self, *, tokens_mlp_dims, channels_mlp_dims):
         super(MixerBlock, self).__init__()
 
         self.tokens_mlp_dims = tokens_mlp_dims
         self.channels_mlp_dims = channels_mlp_dims
 
+    @property
+    def suffix(self):
+        return 'mixer'
+
     def as_dict(self, prefix):
         return {
-            # pass
+            **self.mlp_block_1.as_dict(f'{prefix}/mlp_1'),
+            **self.mlp_block_2.as_dict(f'{prefix}/mlp_2')
         }
 
     def build(self, input_shapes):
         self.rearrange = Reshape([input_shapes[-3] * input_shapes[-2], input_shapes[-1]])
-        self.layer_norm_1 = LayerNormalization(center=False, scale=False)
         self.mlp_block_1 = MlpBlock(self.tokens_mlp_dims)
-        self.layer_norm_2 = LayerNormalization(center=False, scale=False)
         self.mlp_block_2 = MlpBlock(self.channels_mlp_dims)
         self.restore = Reshape(input_shapes[1:])
 
@@ -72,13 +75,11 @@ class MixerBlock(tf.keras.layers.Layer):
 
             z = self.rearrange(x)
 
-            y = self.layer_norm_1(z, training=training)
-            y = tf.transpose(y, [0, 2, 1])
-            y = self.mlp_block_1(y)
+            y = tf.transpose(z, [0, 2, 1])
+            y = self.mlp_block_1(y, training=training, is_recomputing=is_recomputing)
             y = tf.transpose(y, [0, 2, 1])
             z = tf.nn.relu(z + y)
-            y = self.layer_norm_2(z, training=training)
-            y = self.mlp_block_2(y)
+            y = self.mlp_block_2(z, training=training, is_recomputing=is_recomputing)
 
             return tf.nn.relu(x + self.restore(y))
 
